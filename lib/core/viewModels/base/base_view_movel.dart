21d73c0079714e3f38f030c646a99ffebeb658e1
Dechasa <Dechu373@protonmail.com>
2024-08-26 23:57:23 +0300

    readme and commits

--- lib/core/viewModels/base/base_view_movel.dart
+++ lib/core/viewModels/base/base_view_movel.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../../constants/enums.dart';
-
+/// this class abstracts the common attributes of all
+/// out view models. including the states and the methods
+/// that set the state and notify listeners.
 class BaseViewModel extends ChangeNotifier {
   ViewState _state = ViewState.idle;
 
